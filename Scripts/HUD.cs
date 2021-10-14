b19e16e93a2e92418cdd52121e46b3c109852ed1
Mike Calligaro <mikecal@microsoft.com>
2021-10-13 17:05:20 -0700

    Basic Level Infrastructure

--- Scripts/HUD.cs
+++ Scripts/HUD.cs
@@ -3,14 +3,24 @@ using System;
 
 public class HUD : CanvasLayer
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    const string c_level = "Level: ";
+    const string c_villains = "Villains: ";
+    const string c_pr = "PR: ";
+    const string c_separator = "    ";
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         
+    }
+
+    // level is one based
+    // villains is number of villains remaining in the level
+    // pr is villains hit - innocents hit 
+    public void SetScore(int level, int villains, int pr)
+    {
+        string score = c_level + level + c_separator + c_villains + villains + c_separator + c_pr + pr;
+        GetNode<Label>("Score").Text = score;
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,28 +3,58 @@ using System;
 
 public class Main : Node
 {
-    // Totals for the game
+    private int[] villainLevels = {1, 1, 3};
+    private int[] innocentLevels = {0, 1, 6};
+    
+    // Values for the current level
     // Villains are how many bad guys we spawned
     // VillainsHit are how many we dispatched
     // Innocents are how many people who need to be protected we spawned
     // InnocentsHit are how many we let be dispatched (bad) 
-    public int totalNumVillains_ = 0;
-    public int totalNumVillainsHit_ = 0;
-    public int totalNumInnocents_ = 0;
-    public int totalNumInnocentsHit_ = 0;
+    private int levelNumVillains_ = 0;
+    private int levelNumVillainsHit_ = 0;
+    private int levelNumInnocents_ = 0;
+    private int levelNumInnocentsHit_ = 0;
 
-    // Values for the current level
-    public int levelNumVillains_ = 0;
-    public int levelNumVillainsHit_ = 0;
-    public int levelNumInnocents_ = 0;
-    public int levelNumInnocentsHit_ = 0;
+    // Totals for the game
+    private int totalNumVillainsHit_ = 0;
+    private int totalNumInnocentsHit_ = 0;
 
+    private int pr_ = 0;
+
+    private int currentLevel_ = 0;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        // Temporary for testing.
+        SetLevel(0);
     }
+
+    private void SetLevel(int level)
+    {
+        currentLevel_ = level;
+        levelNumVillains_ = villainLevels[level];
+        levelNumVillainsHit_ = 0;
+        levelNumInnocents_ = innocentLevels[level];
+        levelNumInnocentsHit_ = 0;
+
+        CalcPR();
+        UpdateScore();
+    }
+
+    private void CalcPR()
+    {
+        pr_ = totalNumVillainsHit_ - totalNumInnocentsHit_;
+    }
+
+    private void UpdateScore()
+    {        
+        int remainingVillains = levelNumVillains_ - levelNumVillainsHit_;
+        GetNode<HUD>("HUD").SetScore(currentLevel_ + 1, remainingVillains, pr_);
+    }
+
+
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
