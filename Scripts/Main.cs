979ff8b7fcfc0629e318b9c1d3894f73e44a11e0
Mike Calligaro <mikecal@microsoft.com>
2021-10-13 17:24:58 -0700

    Add hit functions

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -6,17 +6,14 @@ public class Main : Node
     private int[] villainLevels = {1, 1, 3};
     private int[] innocentLevels = {0, 1, 6};
     
-    // Values for the current level
-    // Villains are how many bad guys we spawned
+ 
+    // Villains are how many bad guys are left
     // VillainsHit are how many we dispatched
-    // Innocents are how many people who need to be protected we spawned
+    // Innocents are how many people who need to be protected left
     // InnocentsHit are how many we let be dispatched (bad) 
     private int levelNumVillains_ = 0;
-    private int levelNumVillainsHit_ = 0;
     private int levelNumInnocents_ = 0;
-    private int levelNumInnocentsHit_ = 0;
 
-    // Totals for the game
     private int totalNumVillainsHit_ = 0;
     private int totalNumInnocentsHit_ = 0;
 
@@ -29,18 +26,16 @@ public class Main : Node
     {
         // Temporary for testing.
         SetLevel(0);
+        
+        CalcPR();
+        UpdateScore();
     }
 
     private void SetLevel(int level)
     {
         currentLevel_ = level;
         levelNumVillains_ = villainLevels[level];
-        levelNumVillainsHit_ = 0;
         levelNumInnocents_ = innocentLevels[level];
-        levelNumInnocentsHit_ = 0;
-
-        CalcPR();
-        UpdateScore();
     }
 
     private void CalcPR()
@@ -50,11 +45,31 @@ public class Main : Node
 
     private void UpdateScore()
     {        
-        int remainingVillains = levelNumVillains_ - levelNumVillainsHit_;
-        GetNode<HUD>("HUD").SetScore(currentLevel_ + 1, remainingVillains, pr_);
+        GetNode<HUD>("HUD").SetScore(currentLevel_ + 1, levelNumVillains_, pr_);
     }
 
+    private void HitVillain()
+    {
+        totalNumVillainsHit_++;
+        levelNumVillains_--;
+        CalcPR();
+        UpdateScore();
 
+        if (levelNumVillains_ <= 0)
+        {
+            SetLevel(currentLevel_ + 1);
+        }
+
+    }
+
+    private void HitInnocent()
+    {
+        totalNumInnocentsHit_++;
+        levelNumInnocents_--;
+
+        CalcPR();
+        UpdateScore();
+    }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
