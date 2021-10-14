de1764a82303e6c2a3922017e0b80fd44e0d7f34
Amanda Velasco Gallardo <amandave@microsoft.com>
2021-10-14 14:59:39 -0700

    Changed Hero to KinematicBody2D but following in the collider's path infinitely :(

--- Scripts/Hero.cs
+++ Scripts/Hero.cs
@@ -1,13 +1,21 @@
 using Godot;
 using System;
 
-public class Hero : RigidBody2D
+public class Hero : KinematicBody2D
 {
+		// Variables
+		public bool IsPlayerDisguised = false;
+		public bool IsPlayerDefending = false;
+		public int Speed = 200;
+		[Export]
+		public int SpriteMargin = 25; // Margin to help with clamping so hero stays on screen.
+	
+		private Vector2 screenSize_;
 
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
-				
+				screenSize_ = GetViewport().Size;
 		}
 		
 		public override void _PhysicsProcess(float delta)
@@ -15,15 +23,58 @@ public class Hero : RigidBody2D
 			var spaceState = GetWorld2d().DirectSpaceState;
 			var rayCast = GetNode<RayCast2D>("RayCast2D");
 			if (rayCast.IsColliding())
+			// If the raycast collides with something, move towards it
 			{
-				GD.Print("Hit " + rayCast.GetCollider() + " at point: " + rayCast.GetCollisionPoint());
+				var direction = (GlobalPosition - rayCast.GetCollisionPoint()).Normalized();
+				direction = new Vector2(
+					x: Mathf.Clamp(direction.x, 0 + SpriteMargin, screenSize_.x - SpriteMargin),
+					y: Mathf.Clamp(direction.y, 0 + SpriteMargin, screenSize_.y - SpriteMargin)
+				);
+				var collision = MoveAndCollide(direction * delta);
+				if (collision != null)
+				{
+					GD.Print("Hero hit " + collision.Collider);
+				}
+			}
+			else
+			// Rotate the hero until the raycast collides with something
+			{
+				Rotation += 1 * delta;
+			}
+		} 
+		
+		public void FlipIsPlayerDisguised()
+		{
+			IsPlayerDisguised = !IsPlayerDisguised;
+		}
+		
+		public void FlipIsPlayerDefending()
+		{
+			IsPlayerDefending = !IsPlayerDefending;
+		}
+		
+		public void EditRayCastExceptions(Node obj)
+		{
+			var rayCast = GetNode<RayCast2D>("RayCast2D");
+			if (IsPlayerDisguised)
+			{
+				rayCast.RemoveException(obj);
+			} else {
+				rayCast.AddException(obj);
+			}
+			
+			if (IsPlayerDefending)
+			{
+				rayCast.Enabled = false;
+			}
+			else
+			{
+				rayCast.Enabled = true;
 			}
 		}
 		
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+		public void AddToRayCastExceptions(Node node)
+		{
+			GetNode<RayCast2D>("RayCast2D").AddException(node);
+		}
 }
